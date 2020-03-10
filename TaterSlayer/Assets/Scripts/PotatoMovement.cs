0f55ba18cc263d7ea23dd68ea93c04fb48e72709
DaiDai19 <daidai19@users.noreply.github.com>
2020-03-09 20:14:33 -0400

    player controls (movement and jump)

---
+++ TaterSlayer/Assets/Scripts/PotatoMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotatoMovement : MonoBehaviour
+{
+    [Header("PlayerMovement")]
+
+    public float speed = 5f;
+
+    private Rigidbody rb;
+    private Animator anim;
+    private RaycastHit hit;
+    private BoxCollider col;
+
+    [Space]
+    [Header("Other Things")]
+
+    public LayerMask layer;
+    public float hitDistance;
+
+    private bool isGrounded;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hitDistance = GetComponent<Collider>().bounds.extents.y;
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        col = GetComponent<BoxCollider>();
+        hit = GetComponent<RaycastHit>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+        PlayerJump();
+        CheckGround();
+    }
+
+    private void PlayerMove()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector3(moveHorizontal * speed, rb.velocity.y, rb.velocity.z);
+
+    }
+
+    private void PlayerJump()
+    {
+        //float jumpVelocity = 2f;
+        if (isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GetComponent<Rigidbody>().AddForce(Vector3.up * 200);
+            }
+        }
+    }
+
+    private void CheckGround()
+    {
+        if(!Physics.Raycast(transform.position, -Vector3.up, hitDistance + .1f))
+        {
+            isGrounded = false;
+        }
+        else
+        {
+            isGrounded = true;
+        }
+    }
+
+
+    //private bool isGrounded()
+    //{
+    //    RaycastHit hit = Physics.Raycast(col.bounds.center, col.bounds.size, 0f, -Vector3.up, .1f, layer);
+
+    //    return hit.collider != null;
+    //}
+}
